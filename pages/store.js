c95812b0917662f71df2a808f697abb292de862c
System Administrator <root@Peters-MacBook-Pro.local>
2021-04-24 13:39:37 +0100

    fixed nav style issue

--- pages/store.js
+++ pages/store.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 const StorePage = () => {
     return (
         <Layout>
-            <iframe title="Beats" src="https://player.beatstars.com/?storeId=84621"scrolling="no" width="100%" height="800" style={{maxWidth: "100%"}}frameBorder="0" marginHeight="0" marginWidth="0"> -- none -- </iframe>
+            <iframe title="Beats" src="https://player.beatstars.com/?storeId=84621"scrolling="no" width="100%" height="800" style={{maxWidth: "100%", marginBottom: "30px"}}frameBorder="0" marginHeight="0" marginWidth="0"> -- none -- </iframe>
         </Layout>
     )
 }
